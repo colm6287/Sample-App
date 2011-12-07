3fb1471129cec16279e56af8a8253ac87507eea3
Colm Cahill <colmcahill@gmail.com>
2011-12-06 20:25:54 -0800

    Finished layout and routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
-  .
-  .
-  .
+  
   def help
     @title = "Help"
   end
+
+   def home
+    @title = "My Rails App"
+   end
+
+   def about
+    @title = "About My Rails App"
+   end
 end
 
